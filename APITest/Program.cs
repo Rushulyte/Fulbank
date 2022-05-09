955ec0707addc52933296d83e75a6a0127c37b57
Sigmnaificient <edhyjox@gmail.com>
2022-05-09 09:39:32 -0700

    :sparkles: Coinbase API Call

---
+++ APITest/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var response = client.GetAsync("https://api.coinbase.com/v2/exchange-rates?currency=BTC").Result;
+        
+var str = response.Content.ReadAsStringAsync().Result;
+
+JObject json = JObject.Parse(str);
+
+float btcVal = (float)(json["data"]?["rates"]?["EUR"] ?? 0.0);
+Console.Write(btcVal);
+
