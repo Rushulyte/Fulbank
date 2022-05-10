fd471731644ce91b4c132d0f676e80b070c104a6
ReyFey <vic.betsch@gmail.com>
2022-05-10 02:03:53 +0200

    :card_file_box: Update database seeder

--- db/seeder.sql
+++ db/seeder.sql
@@ -16,90 +16,94 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `accountType`
+-- Table structure for table `AccountType`
 --
 
-DROP TABLE IF EXISTS `accountType`;
+DROP DATABASE IF EXISTS fulbank;
+CREATE DATABASE IF NOT EXISTS fulbank;
+USE fulbank;
+
+DROP TABLE IF EXISTS `AccountType`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `accountType` (
+CREATE TABLE `AccountType` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `interest_factor` float NOT NULL DEFAULT 1,
+  `interest` float NOT NULL DEFAULT 1,
+  `ceil` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `accountType`
+-- Dumping data for table `AccountType`
 --
 
-LOCK TABLES `accountType` WRITE;
-/*!40000 ALTER TABLE `accountType` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accountType` ENABLE KEYS */;
+LOCK TABLES `AccountType` WRITE;
+/*!40000 ALTER TABLE `AccountType` DISABLE KEYS */;
+/*!40000 ALTER TABLE `AccountType` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `bankAccount`
+-- Table structure for table `BankAccount`
 --
 
-DROP TABLE IF EXISTS `bankAccount`;
+DROP TABLE IF EXISTS `BankAccount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bankAccount` (
+CREATE TABLE `BankAccount` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `balance` float DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `account_type` int(11) DEFAULT NULL,
+  `balance` float DEFAULT NULL,
   `iban` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `bic` varchar(8) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `bank_account_account_type_id_fk` (`account_type`),
   KEY `bank_account_user_id_fk` (`user_id`),
-  CONSTRAINT `bank_account_account_type_id_fk` FOREIGN KEY (`account_type`) REFERENCES `accountType` (`id`),
+  CONSTRAINT `bank_account_account_type_id_fk` FOREIGN KEY (`account_type`) REFERENCES `AccountType` (`id`),
   CONSTRAINT `bank_account_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bankAccount`
+-- Dumping data for table `BankAccount`
 --
 
-LOCK TABLES `bankAccount` WRITE;
-/*!40000 ALTER TABLE `bankAccount` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bankAccount` ENABLE KEYS */;
+LOCK TABLES `BankAccount` WRITE;
+/*!40000 ALTER TABLE `BankAccount` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BankAccount` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `conversion`
+-- Table structure for table `Conversion`
 --
 
-DROP TABLE IF EXISTS `conversion`;
+DROP TABLE IF EXISTS `Conversion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `conversion` (
+CREATE TABLE `Conversion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `target_wallet` int(11) DEFAULT NULL,
   `account_from` int(11) DEFAULT NULL,
+  `target_Wallet` int(11) DEFAULT NULL,
   `amount` float DEFAULT NULL,
-  `conversion_rate` float DEFAULT NULL,
+  `rate` float DEFAULT NULL,
   `created_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `conversion_bankAccount_id_fk` (`account_from`),
-  KEY `conversion_wallet_id_fk` (`target_wallet`),
-  CONSTRAINT `conversion_bankAccount_id_fk` FOREIGN KEY (`account_from`) REFERENCES `bankAccount` (`id`),
-  CONSTRAINT `conversion_wallet_id_fk` FOREIGN KEY (`target_wallet`) REFERENCES `wallet` (`id`)
+  KEY `Conversion_BankAccount_id_fk` (`account_from`),
+  KEY `Conversion_Wallet_id_fk` (`target_Wallet`),
+  CONSTRAINT `Conversion_BankAccount_id_fk` FOREIGN KEY (`account_from`) REFERENCES `BankAccount` (`id`),
+  CONSTRAINT `Conversion_Wallet_id_fk` FOREIGN KEY (`target_Wallet`) REFERENCES `Wallet` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `conversion`
+-- Dumping data for table `Conversion`
 --
 
-LOCK TABLES `conversion` WRITE;
-/*!40000 ALTER TABLE `conversion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `conversion` ENABLE KEYS */;
+LOCK TABLES `Conversion` WRITE;
+/*!40000 ALTER TABLE `Conversion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Conversion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -111,8 +115,7 @@ DROP TABLE IF EXISTS `CryptoCurrency`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `CryptoCurrency` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` int(11) DEFAULT NULL,
-  `api_symbol` int(11) NOT NULL,
+  `name` varchar(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -135,13 +138,13 @@ DROP TABLE IF EXISTS `ExternalOperation`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ExternalOperation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `amount` float DEFAULT NULL,
-  `target_rib` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
   `account_from` int(11) DEFAULT NULL,
+  `target_rib` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `ExternalOperation_bankAccount_id_fk` (`account_from`),
-  CONSTRAINT `ExternalOperation_bankAccount_id_fk` FOREIGN KEY (`account_from`) REFERENCES `bankAccount` (`id`)
+  KEY `ExternalOperation_BankAccount_id_fk` (`account_from`),
+  CONSTRAINT `ExternalOperation_BankAccount_id_fk` FOREIGN KEY (`account_from`) REFERENCES `BankAccount` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -158,113 +161,116 @@ UNLOCK TABLES;
 -- Table structure for table `operation`
 --
 
-DROP TABLE IF EXISTS `operation`;
+DROP TABLE IF EXISTS `InternalOperation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `operation` (
+CREATE TABLE `InternalOperation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `amount` float NOT NULL,
   `author_id` int(11) DEFAULT NULL,
   `target_id` int(11) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
+  `amount` float NOT NULL,
+  `created_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `operation_bankAccount_id_fk` (`target_id`),
-  KEY `operation_bankAccount_id_fk_2` (`author_id`),
-  CONSTRAINT `operation_bankAccount_id_fk` FOREIGN KEY (`target_id`) REFERENCES `bankAccount` (`id`),
-  CONSTRAINT `operation_bankAccount_id_fk_2` FOREIGN KEY (`author_id`) REFERENCES `bankAccount` (`id`)
+  KEY `InternalOperation_BankAccount_id_fk` (`target_id`),
+  KEY `InternalOperation_BankAccount_id_fk_2` (`author_id`),
+  CONSTRAINT `InternalOperation_BankAccount_id_fk` FOREIGN KEY (`target_id`) REFERENCES `BankAccount` (`id`),
+  CONSTRAINT `InternalOperation_BankAccount_id_fk_2` FOREIGN KEY (`author_id`) REFERENCES `BankAccount` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `operation`
+-- Dumping data for table `InternalOperation`
 --
 
-LOCK TABLES `operation` WRITE;
-/*!40000 ALTER TABLE `operation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `operation` ENABLE KEYS */;
+LOCK TABLES `InternalOperation` WRITE;
+/*!40000 ALTER TABLE `InternalOperation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `InternalOperation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user`
+-- Table structure for table `User`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `User`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_type` int(11) DEFAULT NULL,
   `firstname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lastname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `authentication_string` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `salt` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_type` int(11) DEFAULT NULL,
+  `phone` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `salt` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_userType_id_fk` (`user_type`),
-  CONSTRAINT `user_userType_id_fk` FOREIGN KEY (`user_type`) REFERENCES `userType` (`id`)
+  KEY `User_UserType_id_fk` (`User_type`),
+  CONSTRAINT `User_UserType_id_fk` FOREIGN KEY (`User_type`) REFERENCES `UserType` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `User`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `userType`
+-- Table structure for table `UserType`
 --
 
-DROP TABLE IF EXISTS `userType`;
+DROP TABLE IF EXISTS `UserType`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userType` (
+CREATE TABLE `UserType` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
   `role` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `userType`
+-- Dumping data for table `UserType`
 --
 
-LOCK TABLES `userType` WRITE;
-/*!40000 ALTER TABLE `userType` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userType` ENABLE KEYS */;
+LOCK TABLES `UserType` WRITE;
+/*!40000 ALTER TABLE `UserType` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UserType` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `wallet`
+-- Table structure for table `Wallet`
 --
 
-DROP TABLE IF EXISTS `wallet`;
+DROP TABLE IF EXISTS `Wallet`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wallet` (
+CREATE TABLE `Wallet` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `balance` float DEFAULT NULL,
-  `crypto_currency` int(11) DEFAULT NULL,
   `owner_id` int(11) DEFAULT NULL,
+  `crypto_currency` int(11) DEFAULT NULL,
+  `balance` float DEFAULT NULL,
+  `created_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `wallet_user_id_fk` (`owner_id`),
-  KEY `wallet_CryptoCurrency_id_fk` (`crypto_currency`),
-  CONSTRAINT `wallet_CryptoCurrency_id_fk` FOREIGN KEY (`crypto_currency`) REFERENCES `CryptoCurrency` (`id`),
-  CONSTRAINT `wallet_user_id_fk` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)
+  KEY `Wallet_User_id_fk` (`owner_id`),
+  KEY `Wallet_CryptoCurrency_id_fk` (`crypto_currency`),
+  CONSTRAINT `Wallet_CryptoCurrency_id_fk` FOREIGN KEY (`crypto_currency`) REFERENCES `CryptoCurrency` (`id`),
+  CONSTRAINT `Wallet_User_id_fk` FOREIGN KEY (`owner_id`) REFERENCES `User` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `wallet`
+-- Dumping data for table `Wallet`
 --
 
-LOCK TABLES `wallet` WRITE;
-/*!40000 ALTER TABLE `wallet` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wallet` ENABLE KEYS */;
+LOCK TABLES `Wallet` WRITE;
+/*!40000 ALTER TABLE `Wallet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Wallet` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
