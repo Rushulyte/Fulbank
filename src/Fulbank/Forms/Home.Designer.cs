c3c663ddcbee8d75852e3eafa25a1f655054f7c5
Sigmanificient <sigmanificient@gmail.com>
2022-05-12 12:35:09 -0700

    :recycle: Rename First form to Home

--- src/Fulbank/Forms/Home.Designer.cs
+++ src/Fulbank/Forms/Home.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fulbank
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -32,7 +32,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Home";
         }
 
         #endregion
