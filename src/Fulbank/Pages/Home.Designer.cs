1f51447b258d0188f53454a246883b9284ed1b8b
ReyFey <vic.betsch@gmail.com>
2022-05-12 23:49:24 +0100

    :bug: Fix inexisting element

--- src/Fulbank/Pages/Home.Designer.cs
+++ src/Fulbank/Pages/Home.Designer.cs
@@ -36,12 +36,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Home";
             this.Text = "Home";
             this.ResumeLayout(false);
         }
-
-        private System.Windows.Forms.Panel Panel;
 
         #endregion
     }
